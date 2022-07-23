2bb1aa9018e6eef1ee777984f6175cd55971b276
Kartseva Mariya <kartsevaSP@yandex.ru>
2022-07-23 23:59:21 +0300

    if fixed

--- game.js
+++ game.js
@@ -23,7 +23,12 @@
             console.log(compChoose);
             let userChoose = prompt('Камень, ножницы, бумага ?');
             if(userChoose === null)
-                confirm("Вы хотите закончить?") ? alert('Игра окончена') : start();
+                if(confirm("Вы хотите закончить?"))
+                {
+                    alert('Игра окончена');
+                    return;
+                }
+                    else start();
             else {
                 userChoose = userChoose.toLowerCase().slice(0,1);
                 if(userChoose !== figuries.ru[0].slice(0,1) && userChoose !== figuries.ru[1].slice(0,1) && userChoose !== figuries.ru[2].slice(0,1)) {
@@ -46,7 +51,7 @@
                                 msg = "Выиграл компьютер";
                                 result.computer += 1;
                             }
-                    }
+                        }
 
                 alert(`Компьютер: ${result.computer} \nИгрок: ${result.user} \n${msg}`)
                 start();
