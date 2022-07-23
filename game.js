8e10d0a553fd49a46714e73d2a4c543a6266bba4
Kartseva Mariya <kartsevaSP@yandex.ru>
2022-07-23 23:55:11 +0300

    if fixed

--- game.js
+++ game.js
@@ -1,7 +1,10 @@
 'use strict';
 (() => {
-    const FIGURES_ENG = ['rock', 'scissors', 'papper'];
-    const FIGURES_RU = ['камень', 'ножницы', 'бумага'];
+
+    const figuries = {
+        eng: ['rock', 'scissors', 'papper'],
+        ru: ['камень', 'ножницы', 'бумага'],
+    };
 
     const getRandomIntInclusive = (min, max) => {
         min = Math.ceil(min);
@@ -13,53 +16,38 @@
             user:0,
             computer: 0,
         }
-
         return function start() {
             let msg = '';
             let rez = 0;
-            const compChoose = FIGURES_RU[getRandomIntInclusive(0, FIGURES_RU.length - 1)].slice(0,1);
+            const compChoose = figuries.ru[getRandomIntInclusive(0, figuries.ru.length - 1)].slice(0,1);
             console.log(compChoose);
             let userChoose = prompt('Камень, ножницы, бумага ?');
             if(userChoose === null)
                 confirm("Вы хотите закончить?") ? alert('Игра окончена') : start();
             else {
-                userChoose = userChoose.toLowerCase().slice(0,1)
-                if(userChoose !== 'к' && userChoose !== 'н' && userChoose !== 'б') {
+                userChoose = userChoose.toLowerCase().slice(0,1);
+                if(userChoose !== figuries.ru[0].slice(0,1) && userChoose !== figuries.ru[1].slice(0,1) && userChoose !== figuries.ru[2].slice(0,1)) {
                         start();
                 }
                 if(compChoose === userChoose) {
                     msg = "Ничья";
                 }
                 else {
-                    if(userChoose === 'к') {
-                        if(compChoose === 'н')
-                            rez = 1;
+                    if((userChoose === figuries.ru[0].slice(0,1) && compChoose === figuries.ru[1].slice(0,1)) ||
+                        (userChoose === figuries.ru[1].slice(0,1) && compChoose === figuries.ru[2].slice(0,1))||
+                        (userChoose === figuries.ru[2].slice(0,1) && compChoose === figuries.ru[0].slice(0,1)))
+                            {
+                                msg = "Вы выиграли";
+                                result.user += 1;
+                            }
+
                         else
-                            rez = 2;
+                            {
+                                msg = "Выиграл компьютер";
+                                result.computer += 1;
+                            }
                     }
-                    if(userChoose === 'н') {
-                        if(compChoose === 'б')
-                            rez = 1;
-                        else
-                            rez = 2;
-                    }
-                    if(userChoose === 'б') {
-                        if(compChoose === 'к')
-                            rez = 1;
-                        else
-                            rez = 2;
-                    }
-                }
-                switch (rez) {
-                    case 0: msg = "Ничья";
-                        break;
-                    case 1: msg = "Вы выиграли";
-                        result.user += 1;
-                        break;
-                    case 2: msg = "Выиграл компьютер";
-                        result.computer += 1;
-                        break;
-                }
+
                 alert(`Компьютер: ${result.computer} \nИгрок: ${result.user} \n${msg}`)
                 start();
             }
